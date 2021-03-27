aa512650707a7cb4957cf97ac4b4371eda6e15c4
Jung Ian <jay2u8809@gmail.com>
2021-03-28 01:24:16 +0900

    Fix. Remove Board Domain and Functions

--- src/main/java/com/jayian/businesscard/domain/board/BoarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jayian.businesscard.domain.board;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoarRepository extends JpaRepository<Board, Long> {
-}
